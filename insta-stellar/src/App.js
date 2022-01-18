07a618ac3d43fe9095e1761d97bd6c7d564678e7
qureshe00 <qureshe@mcmaster.ca>
2022-01-17 20:32:50 -0500

    created navbar, home, hero section, fixed position navbar

---
+++ insta-stellar/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import background from "./ActiveSun_NuSTAR_960.jpg";
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+
+    </>
+
+  );
+}
+
+export default App;
---
+++ insta-stellar/src/components/HeroSection/HeroSection.js
@@ -0,0 +1,29 @@
+import { Button } from '@mui/material'
+//import '../App.css';
+import React from 'react'
+import './HeroSection.css';
+
+//public\Worm Hole - 6797.mp4
+//C:\Users\Eesha\InstaStellar00\insta-stellar\public\WormHole.mp4
+function HeroSection() {
+    return (
+        <div className='hero-container'>
+            <video src="../WormHole.mp4" autoPlay loop muted />
+            <h1> text here </h1>
+            <p> more text here </p>
+            <div className='hero-btns'>
+                <Button
+                  className='btns'
+                  buttonStyle='btn--outline'
+                  buttonSize='btn--large' 
+                >
+                    even more text here
+                </Button>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
